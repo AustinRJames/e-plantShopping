95cc1b849007796eafdd538c2b18006a52720d1c
Austin James <arjames1128@gmail.com>
2025-06-29 12:11:44 -0500

    functionality added time to connect Redux Store

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,28 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+      const {name, image, cost} = action.payload;
+
+      const existingItem = state.items.find(item => item.name === name)
+      if (existingItem) {
+        existingItem.quantity++; // If exists just add another in quantity
+      } else {
+        state.items.push({name, image, cost, quantity: 1});
+      }
     
     },
     removeItem: (state, action) => {
+      const { name, quantity} = action.payload
+      state.items = state.items.filter(item => item.name !== name);
+      console.log(state.items);
     },
     updateQuantity: (state, action) => {
-
+      const { name, quantity } = action.payload;
+      // Find item in cart that matches name
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate) {
+        itemToUpdate.quantity = quantity;
+      }
     
     },
   },
