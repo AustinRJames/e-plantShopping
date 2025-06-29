74018ce8d0f0e8c71c2a19a18301aa8760b64b2f
Austin James <arjames1128@gmail.com>
2025-06-29 12:44:22 -0500

    Coming soon alert on checkout button

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -45,6 +45,10 @@ const CartItem = ({ onContinueShopping }) => {
     // dispatch(calculateTotalAmount({name: item.name, quantity: item.quantity}));
   };
 
+  const handleCheckoutButtonClick = () => {
+    alert("Coming Soon");
+  }
+
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
@@ -70,7 +74,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutButtonClick}>Checkout</button>
       </div>
     </div>
   );
